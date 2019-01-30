4c98633cac4ceb22f03cd81a080106ccf688aa6f
Raul Riera <rieraraul@gmail.com>
2019-01-30 13:47:30 -0500

    Refresh the cache

--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -34,7 +34,7 @@ final class MessagesViewController: MSMessagesAppViewController {
 		// Present the view controller for the appropriate presentation style.
 		presentViewController(for: presentationStyle)
 		
-		let key = "UpdatedToTwitterEmoji11.2"
+		let key = "UpdatedToTwitterEmoji11.3"
 		// Run this code only once per "Emoji assets update"
 		if !UserDefaults.standard.bool(forKey: key) {
 			ImageCache.cache.clear()
